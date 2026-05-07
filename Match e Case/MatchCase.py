fb83b2996ea5f5e51f2b7171fb0ffe2c96881b85
nicolecouto16 <nicolecouto338@gmail.com>
2026-05-06 22:34:09 -0300

    add coisas

---
+++ Match e Case/MatchCase.py
@@ -0,0 +1,132 @@
+# comando = 'start'
+
+
+
+# if comando == 'start':
+#     print('iniciar')
+# elif comando == 'stop':
+#     print('parar')
+# else:
+#     print('erro')
+    
+# ###############################################
+
+# match comando:
+#     case "start":
+#         print('inicar')
+#     case 'stop':
+#         print('parar')
+#     case _:
+#         print('erro')
+        
+# ###############################################
+
+# opcao = input("insira 1 ou 2: ")
+
+# match opcao:
+#     case '1':
+#         print('cadastrar')
+#     case '2':
+#         print('listar')
+#     case _:
+#            print("valor invalido")
+           
+# ###############################################
+
+# dados = ['produto', 'arroz', 10]
+
+# match dados:
+#     case ('produto', nome, qtd): #se der match ele iria aparecer o nome e a aqt
+#         print(f"{nome} - {qtd}")
+#     case _:
+#         print("formato invalido")
+           
+# ###############################################   
+
+# match lista:
+#     case [1, 2, 3]:
+#         print("lista completa")
+#     case _:
+#         print("lista incompleta")
+
+# ###############################################
+
+# if tipo == 'A' and status = 'ativo':
+#     print("faça alguma coisa")
+# elif tipo == '8' and status = 'inativo'
+#     print("faça alguma coisa")
+
+# match (lista, status):
+#     case ("A", "ativo"):
+#         print("faça alguma coisa")
+#     case ("8", "inativo"):
+#         print("faça alguma coisa")
+
+# ###############################################
+# numero = int(input("insira um numero: "))
+
+# match numero:
+#     case _ if numero %2 == 0:
+#         print("par")
+#     case _:
+#         print("impar")
+# ###############################################
+        
+# Ex.1 peça para o usuario numeros entre 1 e 7 mostre o dia correspondente
+
+# opcao = input("insira uma numero de 1 a 7: ")
+
+# match opcao:
+#     case '1':
+#         print('segunda')
+#     case '2':
+#         print('terça')
+#     case '3':
+#         print('quarta')
+#     case '4':
+#         print('quarta')
+#     case '5':
+#         print('quinta')
+#     case '6':
+#         print('sexta')
+#     case '7':
+#         print('sabado')
+#     case _:
+#         print("invalido")
+           
+# Ex.2 crie um sistema de menu simples, onde univrsisdade = fiap - print bem
+# vindo a eng de software
+ 
+# universidade = usp - print voce ta ferrado vai precisar estudar muito
+
+# universidade = ITA - print voce é um monstro
+
+# qualquer outra univrsidade, ok
+
+# opcao = input("insira sua universidade ")
+
+# match universidade:
+#     case "fiap":
+#         print('bem vindo a eng de software')
+#     case 'usp':
+#         print(' voce ta ferrado vai precisar estudar muito')
+#     case 'ITA':
+#         print('voce é um monstro')
+#     case _:
+#         print('ok')
+        
+
+
+
+
+
+# Ex.3 crie uma estrutura de entrata para o usuario inserir nota, caso seja maior
+# ou igual a 7, print aprovado, caso seja >=5 recuperação, caso contrario, reprovado
+
+# Ex.4 crie um algoritmo com mach/case e while paa o usuario inserir um 
+# comando, digite sair para encerar
+
+# em caso, sair, print encerrando. caso contrario, comando invalido
+
+        
+
